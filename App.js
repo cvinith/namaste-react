16c825e10930206653a50d57771020ea032b4edf
Vinith <vinith@Viniths-MacBook-Air.local>
2026-05-18 16:13:32 +0530

    code of episode1

---
+++ App.js
@@ -0,0 +1,68 @@
+// const heading = React.createElement("h1", {
+//     id:"heading", xyz:"abc"
+// }, "Hello World From React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+//------------------------------------------------------------------------------------------------------
+
+/* <div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+    <div/>
+<div/> */
+
+
+// const heading = React.createElement("div", { id : "parent"},
+//     React.createElement("div", { id : "child"},React.createElement("h1", {}, "I am h1 tag") )
+//  );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//------------------------------------------------------------------------------------------------------
+
+
+/* <div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    <div/>
+<div/> */
+
+
+// const heading = React.createElement("div", { id: "parent" },
+//     React.createElement("div", { id: "child" },
+//         [React.createElement("h1", {}, "I am h1 tag") ,
+//     React.createElement("h2", {}, "I am h2 tag")
+//     ] )
+//  );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//------------------------------------------------------------------------------------------------------
+
+
+/* <div id="parent">
+    <div id="child">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    <div/>
+        <div id="child2">
+        <h1>I am h1 tag</h1>
+        <h2>I am h2 tag</h2>
+    <div/>
+<div/> */
+
+const heading = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child" },
+        [React.createElement("h1", {}, "I am h1 tag") ,
+    React.createElement("h2", {}, "I am h2 tag")
+    ] ),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 tag") ,
+    React.createElement("h2", {}, "I am h2 tag")
+    ] )]
+ );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
