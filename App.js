9f6dc85cb753026029ead66df4bdcb395caf824c
Vinith <vinith@Viniths-MacBook-Air.local>
2026-05-19 08:32:36 +0530

    ignting app

--- App.js
+++ App.js
@@ -53,6 +53,8 @@
         <h2>I am h2 tag</h2>
     <div/>
 <div/> */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child" },
