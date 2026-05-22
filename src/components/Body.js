a269df2b4ef453a35f631bed92d2c8a7e70afca1
Vinith <vinith@Viniths-MacBook-Air.local>
2026-05-22 15:54:10 +0530

    Added top rated resturant feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,20 @@
 import ResturantCard from "./ResturantCard";
-import restList from "../utils/mockdata"
+import restList from "../utils/mockdata";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfResturants, setListOfResturants] = useState(restList);
+
+  const filterRes = () =>{
+    const filterList = restList.filter((res)=>res.info.avgRating>4);
+    setListOfResturants(filterList);
+
+  }
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <button className="filter-btn" onClick={filterRes}>Top Rated Resturants</button>
       <div className="res-container">
-        {restList.map((res) => {
+        {listOfResturants.map((res) => {
           return <ResturantCard resData={res} key={res.info.id} />;
         })}
       </div>
